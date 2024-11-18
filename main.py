dbffecb3ef98ab650a3335478d5bf74591732715
ANGELO ALFREDO HAFNER <57808722+angelohafner@users.noreply.github.com>
2024-11-18 17:07:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from input_data import texto_1, texto_2
 def main():
     st.title("Proteção diferencial de tensão em banco fuseless com ligação em estrela dividida e aterrado")
     st.write(texto_1)
-    st.image("Figura_26_ieee37p99.png", caption="Figura 26 IEEE39.99", use_container_width=True)
+    st.image("Figura_26_ieee37p99.png", caption="Figura 26 IEEE37.99", use_container_width=True)
     st.write(texto_2)
     st.sidebar.header("Parâmetros de Entrada")
 
